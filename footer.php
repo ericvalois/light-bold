b31756ed63abd59acb64b5d4094d63d4305bf77a
Eric Valois <eric.v@bulledev.com>
2016-05-06 14:51:58 -0400

    Hide first footer if all sidebar are empty

--- footer.php
+++ footer.php
@@ -11,41 +11,45 @@
 
 ?>
 
+	
+	<?php if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) || is_active_sidebar( 'footer-3' ) || is_active_sidebar( 'footer-4' ) ) : ?>
+		<footer class="site-footer clearfix dark-bg" role="contentinfo">
 
-	<footer class="site-footer clearfix dark-bg" role="contentinfo">
+			<div class="clearfix mt3 mb3 lg-mt4 px2 lg-px3 ">
+				<div class="col col-12 md-col-3">
+					<?php dynamic_sidebar( 'footer-1' ); ?>
+				</div>
 
-		<div class="clearfix mt3 mb3 lg-mt4 px2 lg-px3 ">
-			<div class="col col-12 md-col-3">
-				<?php dynamic_sidebar( 'footer-1' ); ?>
+				<div class="col col-12 md-col-3">
+					<?php dynamic_sidebar( 'footer-2' ); ?>
+				</div>
+
+				<div class="col col-12 md-col-3">
+					<?php dynamic_sidebar( 'footer-3' ); ?>
+				</div>
+
+				<div class="col col-12 md-col-3">
+					<?php dynamic_sidebar( 'footer-4' ); ?>
+				</div>
 			</div>
 
-			<div class="col col-12 md-col-3">
-				<?php dynamic_sidebar( 'footer-2' ); ?>
-			</div>
+		</footer><!-- #colophon -->
 
-			<div class="col col-12 md-col-3">
-				<?php dynamic_sidebar( 'footer-3' ); ?>
-			</div>
+	<?php endif; ?>
 
-			<div class="col col-12 md-col-3">
-				<?php dynamic_sidebar( 'footer-4' ); ?>
-			</div>
+	<div class="site-info py2 px2 lg-px3 bg-black clearfix">
+		<div class="col col-12 md-col-6 mb1 md-mb0">
+			<div class="white-color copy">@ <?php echo date("Y"); ?> <?php echo get_field("perf_footer_copy","option"); ?></div>
 		</div>
-
-		<div class="site-info py2 px2 lg-px3 bg-black clearfix">
-			<div class="col col-12 md-col-6 mb1 md-mb0">
-				<div class="white-color copy">@ <?php echo date("Y"); ?> <?php echo get_field("perf_footer_copy","option"); ?></div>
-			</div>
+		
+		<div class="col col-12 md-col-6">
+			<?php if( !get_field("perf_disble_top_anchor") ){ echo '<button id="to_top" class="right ml2 bg-black border-none"><i class="fa fa-chevron-up"></i></button>'; } ?>
+			<?php 
+				wp_nav_menu( array( 'theme_location' => 'sub-footer', 'menu_id' => 'sub-footer', 'container' => '', 'fallback_cb' => false, 'menu_class' => 'list-reset md-right' ) );
+			?>
 			
-			<div class="col col-12 md-col-6">
-				<?php if( !get_field("perf_disble_top_anchor") ){ echo '<button id="to_top" class="right ml2 bg-black border-none"><i class="fa fa-chevron-up"></i></button>'; } ?>
-				<?php 
-					wp_nav_menu( array( 'theme_location' => 'sub-footer', 'menu_id' => 'sub-footer', 'container' => '', 'fallback_cb' => false, 'menu_class' => 'list-reset md-right' ) );
-				?>
-				
-			</div>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+		</div>
+	</div><!-- .site-info -->
 
 </div><!-- .site-content -->
 
