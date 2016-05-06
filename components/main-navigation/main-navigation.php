beeff7271d747e4527f04c47ac111a0899bbdd48
Eric Valois <eric.v@bulledev.com>
2016-05-06 14:04:08 -0400

    Clean up

--- components/main-navigation/main-navigation.php
+++ components/main-navigation/main-navigation.php
@@ -1,6 +1,4 @@
 <nav id="nav-container" class="nav-container site-sidebar bg-white">
-	<a class="accessibility skip-link" href="#content">Aller au contenu principal</a>
+	<a class="accessibility skip-link" href="#content"><?php _e("Skip to main content","perf"); ?></a>
 	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'container' => '', 'menu_class' => 'list-reset', 'items_wrap' => perf_add_search_menu(), 'walker' => new perf_Walker_Nav_Menu() ) ); ?>
-	<?php //clean_custom_menu("primary"); ?>
-	<?php // https://wordpress.org/support/topic/menu-icon-with-custom-menu-function ?>
 </nav>
