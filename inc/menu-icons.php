09d05862863de0558a6a24cff296554915c13465
Eric Valois <eric.v@bulledev.com>
2016-05-24 13:19:06 -0400

    Load font awesome locally

--- inc/menu-icons.php
+++ inc/menu-icons.php
@@ -32,3 +32,26 @@ add_filter( 'menu_icons_types', 'my_remove_menu_icons_type' );
 
 // Remove menu icon setting
 //add_filter( 'menu_icons_disable_settings', '__return_true' );
+
+
+/**
+ * Load Font Awesome's CSS from CDN
+ *
+ * @param  string                $stylesheet_uri Icon type's stylesheet URI.
+ * @param  string                $icon_type_id   Icon type's ID.
+ * @param  Icon_Picker_Type_Font $icon_type      Icon type's instance.
+ *
+ * @return string
+ */
+function perf_font_awesome_css_from_cdn( $stylesheet_uri, $icon_type_id, $icon_type ) {
+    if ( 'fa' === $icon_type_id ) {
+        $stylesheet_uri = sprintf(
+            get_template_directory_uri() . '/inc/font-awesome/css/font-awesome.min.css',
+            $icon_type->version
+        );
+    }
+
+    return $stylesheet_uri;
+}
+add_filter( 'icon_picker_icon_type_stylesheet_uri', 'perf_font_awesome_css_from_cdn', 10, 3 );
+
