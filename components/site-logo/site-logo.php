f7cca71e679214330f7228a6376fdbeca1fdb4eb
Eric Valois <eric.v@bulledev.com>
2016-06-23 10:17:29 -0400

    Add text logo

--- components/site-logo/site-logo.php
+++ components/site-logo/site-logo.php
@@ -1,3 +1,10 @@
 <div class="site-logo lg-mt0">
-	<a id="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="border-none"></a>
+	<?php if( get_field("perf_log_sm","option") || get_field("perf_log_lg","option") ): ?>
+		<a id="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="border-none"></a>
+	<?php else: ?>
+		<a id="text-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="border-none dark-color small-p block bold">
+			<?php echo get_bloginfo("name"); ?><br>
+			<span class="desc regular sm-show"><?php echo get_bloginfo("description"); ?></span>
+		</a>
+	<?php endif; ?>
 </div>
