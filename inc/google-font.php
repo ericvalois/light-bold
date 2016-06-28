fc9e43a278357495d84e3eef73c8f3c3ebacedb8
Eric Valois <eric.v@bulledev.com>
2016-06-28 14:51:33 -0400

    Fix notice and fout of google font

--- inc/google-font.php
+++ inc/google-font.php
@@ -7,49 +7,52 @@
 	}
 
 	function perf_google_font() {
+
+		$font_face = '';
+
 		switch ( get_field("perf_google_font","option") ) {
 		    case 'open-sans':
-		        $font_familly = "font-family: 'Open Sans', sans-serif;";
+		        $font_familly = "font-family: 'Open Sans', sans-serif !important;";
 		        $temp_css = wp_remote_get("https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700");
     			$font_face .= $temp_css['body'];
 		        break;
 		    case 'josefin-slab':
-		        $font_familly = "font-family: 'Josefin Slab', serif;";
+		        $font_familly = "font-family: 'Josefin Slab', serif !important;";
 		        $temp_css = wp_remote_get("https://fonts.googleapis.com/css?family=Josefin+Slab:400,400italic,700");
     			$font_face .= $temp_css['body'];
 		        break;
 		    case 'arvo':
-		        $font_familly = "font-family: 'Arvo', serif;";
+		        $font_familly = "font-family: 'Arvo', serif !important;";
 		        $temp_css = wp_remote_get("https://fonts.googleapis.com/css?family=Arvo:400,400italic,700");
     			$font_face .= $temp_css['body'];
 		        break;
 		    case 'lato':
-		        $font_familly = "font-family: 'Lato', sans-serif;";
+		        $font_familly = "font-family: 'Lato', sans-serif !important;";
 		        $temp_css = wp_remote_get("https://fonts.googleapis.com/css?family=Lato:400,400italic,700");
     			$font_face .= $temp_css['body'];
 		        break;
 		    case 'vollkorn':
-		        $font_familly = "font-family: 'Vollkorn', serif;";
+		        $font_familly = "font-family: 'Vollkorn', serif !important;";
 		        $temp_css = wp_remote_get("https://fonts.googleapis.com/css?family=Vollkorn:400,400italic,700");
     			$font_face .= $temp_css['body'];
 		        break;
 		    case 'abril-fatface':
-		        $font_familly = "font-family: 'Abril Fatface', cursive;";
+		        $font_familly = "font-family: 'Abril Fatface', cursive !important;";
 		        $temp_css = wp_remote_get("https://fonts.googleapis.com/css?family=Abril+Fatface");
     			$font_face .= $temp_css['body'];
 		        break;
 		    case 'ubuntu':
-		        $font_familly = "font-family: 'Ubuntu', sans-serif;";
+		        $font_familly = "font-family: 'Ubuntu', sans-serif !important;";
 		        $temp_css = wp_remote_get("https://fonts.googleapis.com/css?family=Ubuntu:400,400italic,700");
     			$font_face .= $temp_css['body'];
 		        break;
 		    case 'pt-sans-pt-serif':
-		        $font_familly = "font-family: 'PT Sans', sans-serif;";
+		        $font_familly = "font-family: 'PT Sans', sans-serif !important;";
 		        $temp_css = wp_remote_get("https://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic");
     			$font_face .= $temp_css['body'];
 		        break;
 		    case 'old-standard-tt':
-		        $font_familly = "font-family: 'Old Standard TT', serif;";
+		        $font_familly = "font-family: 'Old Standard TT', serif !important;";
 		        $temp_css = wp_remote_get("https://fonts.googleapis.com/css?family=Old+Standard+TT");
     			$font_face .= $temp_css['body'];
 		        break;
