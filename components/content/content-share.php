0b7691e38552740b1085a4c8ba44f50acd29a571
Eric Valois <eric.v@bulledev.com>
2016-06-28 09:42:57 -0400

    Fix shares

--- components/content/content-share.php
+++ components/content/content-share.php
@@ -11,11 +11,11 @@
 
 <div id="social_widget" class="clearfix mb2 hide-print">
 	<div class="left table mb1 mr1">
-		<a target="_blank" href="http://www.facebook.com/sharer.php?u=<?php echo get_permalink(); ?>&t=<?php echo urlencode( get_the_title() ); ?>" class="table-cell social_share"><i class="fa fa-facebook"></i></a>
+		<a target="_blank" href="http://www.facebook.com/sharer.php?u=<?php echo get_permalink(); ?>&t=<?php echo get_the_title(); ?>" class="table-cell social_share"><i class="fa fa-facebook"></i></a>
 	</div>
 
 	<div class="left table mb1 mr1">
-		<a target="_blank" href="http://twitter.com/home?status=<?php echo urlencode( get_the_title() ); ?>+<?php echo get_permalink(); ?>" class="table-cell social_share"><i class="fa fa-twitter"></i></a>
+		<a target="_blank" href="http://twitter.com/home?status=<?php echo get_the_title(); ?>+<?php echo get_permalink(); ?>" class="table-cell social_share"><i class="fa fa-twitter"></i></a>
 	</div>
 
 	<div class="left table mb1 mr1">
@@ -23,10 +23,10 @@
 	</div>
 
 	<div class="left table mb1 mr1">
-		<a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_permalink(); ?>&title=<?php echo urlencode( get_the_title() ); ?>" class="table-cell social_share"><i class="fa fa-linkedin"></i></a>
+		<a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_permalink(); ?>&title=<?php echo get_the_title(); ?>" class="table-cell social_share"><i class="fa fa-linkedin"></i></a>
 	</div>
 
 	<div class="left table mr1">
-		<a target="_blank" href="http://pinterest.com/pin/create/bookmarklet/?media=article&url=<?php echo get_permalink(); ?>&is_video=false&description=<?php echo urlencode( get_the_title() ); ?>" class="table-cell social_share"><i class="fa fa-pinterest"></i></a>
+		<a target="_blank" href="http://pinterest.com/pin/create/bookmarklet/?media=article&url=<?php echo get_permalink(); ?>&is_video=false&description=<?php echo get_the_title(); ?>" class="table-cell social_share"><i class="fa fa-pinterest"></i></a>
 	</div>
 </div>
