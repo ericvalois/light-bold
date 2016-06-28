3116acc6302f8ceed8a6138eebf17ebd4d33acfa
Eric Valois <eric.v@bulledev.com>
2016-06-28 09:19:54 -0400

    Fix social share

--- components/content/content-share.php
+++ components/content/content-share.php
@@ -11,22 +11,22 @@
 
 <div id="social_widget" class="clearfix mb2 hide-print">
 	<div class="left table mb1 mr1">
-		<a target="_blank" href="<?php echo urlencode('http://www.facebook.com/sharer.php?u=' . get_permalink() . '&t=' . get_the_title() ); ?>" class="table-cell social_share"><i class="fa fa-facebook"></i></a>
+		<a target="_blank" href="http://www.facebook.com/sharer.php?u=<?php echo get_permalink(); ?>&t=<?php echo urlencode( get_the_title() ); ?>" class="table-cell social_share"><i class="fa fa-facebook"></i></a>
 	</div>
 
 	<div class="left table mb1 mr1">
-		<a target="_blank" href="<?php echo urlencode('http://twitter.com/home?status=' . get_the_title() . '+' . get_permalink() ); ?>" class="table-cell social_share"><i class="fa fa-twitter"></i></a>
+		<a target="_blank" href="http://twitter.com/home?status=<?php echo urlencode( get_the_title() ); ?>+<?php echo get_permalink(); ?>" class="table-cell social_share"><i class="fa fa-twitter"></i></a>
 	</div>
 
 	<div class="left table mb1 mr1">
-		<a target="_blank" href="<?php echo urlencode('https://plus.google.com/share?url=' . get_permalink() ); ?>" class="table-cell social_share"><i class="fa fa-google-plus"></i></a>
+		<a target="_blank" href="https://plus.google.com/share?url=<?php echo get_permalink(); ?>" class="table-cell social_share"><i class="fa fa-google-plus"></i></a>
 	</div>
 
 	<div class="left table mb1 mr1">
-		<a target="_blank" href="<?php echo urlencode('https://www.linkedin.com/shareArticle?mini=true&url=' . get_permalink() . '&title=' . get_the_title() ); ?>" class="table-cell social_share"><i class="fa fa-linkedin"></i></a>
+		<a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_permalink(); ?>&title=<?php echo urlencode( get_the_title() ); ?>" class="table-cell social_share"><i class="fa fa-linkedin"></i></a>
 	</div>
 
 	<div class="left table mr1">
-		<a target="_blank" href="<?php echo urlencode('http://pinterest.com/pin/create/bookmarklet/?media=article&url=' . get_permalink() . '&is_video=false&description=' .  get_the_title() ); ?>" class="table-cell social_share"><i class="fa fa-pinterest"></i></a>
+		<a target="_blank" href="http://pinterest.com/pin/create/bookmarklet/?media=article&url=<?php echo get_permalink(); ?>&is_video=false&description=<?php echo urlencode( get_the_title() ); ?>" class="table-cell social_share"><i class="fa fa-pinterest"></i></a>
 	</div>
 </div>
