a47a7b0ec2759a0ffa60fa58cecaca7c8817c923
Eric Valois <eric.v@bulledev.com>
2016-08-05 14:23:48 -0400

    Test incomptability

--- inc/plugins-compatibility.php
+++ inc/plugins-compatibility.php
@@ -6,7 +6,7 @@ if ( class_exists( 'autoptimizeConfig' ) ){
 function sample_admin_notice__error() {
 	?>
     <div class="notice notice-error is-dismissible clear">
-        <p class="clear"><strong>Light & Bold:</strong> <?php _e("Autoptimize is not compatible with Light & Bold and will generate unexpected results."); ?><a href="/wp-admin/admin-post.php?action=deactivate_plugin&plugin=autoptimize%2Fautoptimize.php&_wpnonce=780147b3" class="button-secondary alignright"><?php _e( 'Deactivate', 'rocket' ) ?></a></p>
+        <p class="clear"><strong>Light & Bold:</strong> <?php _e("Autoptimize is not compatible with Light & Bold and will generate unexpected results.", "perf"); ?></p>
     	<br>
     </div>
     <?php
