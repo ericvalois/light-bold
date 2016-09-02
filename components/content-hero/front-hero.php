9af7c4d6ac680360944a6e122ae35055dd998d2c
Eric Valois <eric.v@bulledev.com>
2016-09-02 14:26:18 -0400

    Fix small excessif margin

--- components/content-hero/front-hero.php
+++ components/content-hero/front-hero.php
@@ -21,7 +21,7 @@
       	<div class="md-table-cell md-col-5 dark-bg front-hero-content ">
       		<div class="table col-12 front-hero-content">
       			<div class="px2 md-px3 table-cell align-middle py2">
-      				<h1 class="h3 separator white-color py2"><?php echo get_field("perf_main_title"); ?></h1>
+      				<h1 class="h3 separator white-color mt2"><?php echo get_field("perf_main_title"); ?></h1>
     		  		<p class="small-p mt2 lg-mt3 mb2 lg-mb3 white-color"><?php echo get_field("perf_main_content"); ?></p>
     		  		<a href="<?php echo get_field("perf_main_link"); ?>" class="perf_btn mb3" <?php if( get_field("perf_main_new_window") ){ echo 'target="_blank"'; } ?>><?php echo get_field("perf_main_button_text"); ?></a>
       			</div>
