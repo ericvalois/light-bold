f6ed9b3cde951c51c12e5a9a36a1d5f1c9a4f66a
Eric Valois <eric.v@bulledev.com>
2016-09-23 14:02:08 -0400

    Small fix

--- components/front-sections/hero_section.php
+++ components/front-sections/hero_section.php
@@ -5,46 +5,6 @@
  * @package perfthemes
  */
 ?>
-<?php $perf_hero_img = perf_get_field("image"); ?>
-<?php if( $perf_hero_img ): ?>
-	<section class="clearfix section4 px4 py3 lg-py4 white-color bg-cover bg-center center lazyload" data-sizes="auto"
-		data-bgset="<?php echo $perf_hero_img['sizes']['perfthemes-hero-md']; ?> [(min-width: 60em)] |
-		<?php echo $perf_hero_img['sizes']['perfthemes-hero-sm']; ?> [(min-width: 52em)] |
-		<?php echo $perf_hero_img['sizes']['perfthemes-hero-sm']; ?>">
-<?php endif; ?>
-
-<?php
-	if( function_exists('have_rows')):
-					
-		if( have_rows('perf_front_hero_content') ):
-
-		    while ( have_rows('perf_front_hero_content') ) : the_row();
-
-				if( get_row_layout() == 'custom_content' ):
-
-		        	get_template_part( 'components/front-sections/custom_content' );
-
-		        elseif( get_row_layout() == 'lastest_posts' ): 
-
-		        	get_template_part( 'components/front-sections/lastest_posts' );
-
-		        elseif( get_row_layout() == 'manual_selection' ): 
-
-		        	get_template_part( 'components/front-sections/manual_selection' );
-
-		        endif;
-
-		    endwhile;
-
-		endif;
-
-	endif;
-?>
-
-<?php if( $perf_hero_img ): ?>
-	</section>
-<?php endif; ?>
-
 <?php $perf_hero_img = get_sub_field("image"); ?>
 <?php if( $perf_hero_img ): ?>
 	
