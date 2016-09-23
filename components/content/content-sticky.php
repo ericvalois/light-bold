33409ccbb7a2331903c288419a9ca4151c3a5afe
Eric Valois <eric.v@bulledev.com>
2016-09-23 13:41:48 -0400

    Missiong author link

--- components/content/content-sticky.php
+++ components/content/content-sticky.php
@@ -42,7 +42,7 @@
 
 				<div class="sticky-meta ultra-small upper mb1">
 					<span class="main-color"><?php echo get_the_date(); ?></span>
-					<span class="white-color inline-block px"> | </span><a href="" class="white-color"><?php echo get_the_author(); ?></a>
+					<span class="white-color inline-block px"> | </span><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ), get_the_author_meta( 'user_nicename' ) ); ?>" class="white-color"><?php echo get_the_author(); ?></a>
 				</div>
 
 				<h2 class="entry-title h2 separator mb3 mt0"><a class="white-color" href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
