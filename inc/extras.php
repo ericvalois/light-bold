03f97b39d2d3d39c1b94cb147ca4f9fe0236a6b1
Eric Valois <eric.v@bulledev.com>
2016-09-30 10:07:59 -0400

    Small option page improvment

--- inc/extras.php
+++ inc/extras.php
@@ -359,3 +359,76 @@ function perf_add_flickity_listener(){
     </script>
     <?php
 }
+
+/**
+ * ACF Custom Switch True or False
+ */
+add_action('admin_head', 'perf_custom_switch');
+function perf_custom_switch() {
+  echo '<style>
+    .switch {
+      position: relative;
+      display: inline-block !important;
+      width: 60px;
+      height: 34px;
+    }
+
+    /* Hide default HTML checkbox */
+    .switch input {display:none;}
+
+    /* The slider */
+    .slider {
+      position: absolute;
+      cursor: pointer;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: #ccc;
+      -webkit-transition: .4s;
+      transition: .4s;
+    }
+
+    .slider:before {
+      position: absolute;
+      content: "";
+      height: 26px;
+      width: 26px;
+      left: 4px;
+      bottom: 4px;
+      background-color: white;
+      -webkit-transition: .4s;
+      transition: .4s;
+    }
+
+    input:checked + .slider {
+      background-color: #2196F3;
+    }
+
+    input:focus + .slider {
+      box-shadow: 0 0 1px #2196F3;
+    }
+
+    input:checked + .slider:before {
+      -webkit-transform: translateX(26px);
+      -ms-transform: translateX(26px);
+      transform: translateX(26px);
+    }
+
+    /* Rounded sliders */
+    .slider.round {
+      border-radius: 34px;
+    }
+
+    .slider.round:before {
+      border-radius: 50%;
+    }
+</style>';
+
+echo '<script>
+    jQuery(document).ready(function($) {
+        $(".acf-bl label").addClass("switch");
+        $(".acf-bl label").append( "<div class=slider></div>" );
+    });
+</script>';
+}
