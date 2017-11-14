bc7330f0a09bf93e8a0cfa2ffe0d66dcbf21a5ce
Eric Valois <eric.v@bulledev.com>
2017-11-14 10:46:46 -0500

    Fix - Password protected post not working

--- inc/custom-protected-form.php
+++ inc/custom-protected-form.php
@@ -6,13 +6,12 @@
  */
 add_filter( 'the_password_form', 'light_bold_custom_password_form' );
 function light_bold_custom_password_form() {
-	global $post;
-	$light_bold_label = 'pwbox-'.( empty( $post->ID ) ? rand() : $post->ID );
-	$light_bold_o = '<form class="protected-post-form" action="' . esc_url( get_option('siteurl') ) . '/wp-pass.php" method="post">';
-	
-	$light_bold_o .= '<p>' . esc_html__(  "This content is password protected. To view it please enter your password below:","light-bold" ) . '</p>';
-
-	$light_bold_o .= '<label for="' . esc_attr( $light_bold_label ) . '">' . esc_html__( "Password:","light-bold" ) . ' </label><input name="post_password" id="' . esc_attr( $light_bold_label ) . '" type="password" size="20" /><input type="submit" class="light_bold_btn mt1 mb1" name="Submit" value="' . esc_attr__( "Submit","light-bold" ) . '" />
-	</form>';
-	return $light_bold_o;
+    global $post;
+    $label = 'pwbox-'.( empty( $post->ID ) ? rand() : $post->ID );
+    $o = '<form action="' . esc_url( site_url( 'wp-login.php?action=postpass', 'login_post' ) ) . '" method="post">
+    ' . esc_html__(  "This content is password protected. To view it please enter your password below:","light-bold" ) . '
+    <label for="' . $label . '">' . esc_html__( "Password:","light-bold" ) . ' </label><input name="post_password" id="' . $label . '" type="password" size="20" maxlength="20" /><input class="light_bold_btn mt1 mb1" type="submit" name="Submit" value="' . esc_attr__( "Submit" ) . '" />
+    </form>
+    ';
+    return $o;
 }
