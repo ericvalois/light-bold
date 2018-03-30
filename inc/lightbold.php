aa789f31583e738ac7262eb9b42d666d351bd374
Eric Valois <info@ericvalois.com>
2018-03-30 09:59:04 -0400

    Load json ACF from parent theme

--- inc/lightbold.php
+++ inc/lightbold.php
@@ -759,3 +759,20 @@ function light_bold_search_bar_form(){
 
 <?php
 }
+
+/*
+* Lod ACF field from parent theme
+*/
+add_filter('acf/settings/save_json', function() {
+	return get_stylesheet_directory() . '/acf-json';
+});
+add_filter('acf/settings/load_json', function($paths) {
+	$paths = array(get_template_directory() . '/acf-json');
+
+	if(is_child_theme())
+	{
+		$paths[] = get_stylesheet_directory() . '/acf-json';
+	}
+
+	return $paths;
+});
