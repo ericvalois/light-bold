2eee118a7cef7ad1078238dc1d36db4499da5935
Eric Valois <info@ericvalois.com>
2018-09-27 13:32:46 -0400

    Add retina logo compatibility

--- components/site-logo/site-logo.php
+++ components/site-logo/site-logo.php
@@ -1,11 +1,29 @@
 <div class="site-logo inline-flex flex-center">
 	<?php if( get_field("perf_log_sm","option") || get_field("perf_log_lg","option") ): ?>
+    <?php 
+        $mobile = get_field("perf_log_sm","option");
+        $mobile_retina = get_field("perf_log_sm_retina","option");
+        $desktop = get_field("perf_log_lg","option");
+        $desktop_retina = get_field("perf_log_lg_retina","option");
+    ?>
 		<a id="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="border-none mx-auto">
 			<picture>
-				<source srcset="<?php echo esc_url( get_field("perf_log_sm","option") ); ?>" media="(max-width: 1199px)">
-				<source srcset="<?php echo esc_url( get_field("perf_log_md","option") ); ?>" media="(min-width: 1200px) and (max-width: 1650px)">
-				<source srcset="<?php echo esc_url( get_field("perf_log_lg","option")); ?>" media="(min-width: 1650px)">
- 				<img class="block mx-auto" src="<?php echo esc_url( get_field("perf_log_md","option") ); ?>" alt="Logo">
+				<source srcset="<?php echo esc_url( $mobile ); ?>
+                                <?php
+                                    if( $mobile_retina ){
+                                        echo ", " . esc_url( $mobile_retina ) . " 2x"; 
+                                    }
+                                ?>" 
+                        media="(max-width: 1199px)">
+				<source srcset="<?php echo esc_url( $desktop ); ?>
+                                <?php
+                                    if( $desktop_retina ){
+                                        echo ", " . esc_url( $desktop_retina ) . " 2x"; 
+                                    }
+                                ?> 
+                                " 
+                        media="(min-width: 1200px)">
+ 				<img class="block mx-auto" src="<?php echo esc_url( $desktop ); ?>" alt="Logo">
 			</picture>
 		</a>
 	<?php else: ?>
